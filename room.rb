679c36cdaa51321a923ae624c821f833dd039cd1
derkrisz <krisztian.dergmail.com>
2017-11-10 15:36:31 +0000

    builds room class

---
+++ room.rb
@@ -0,0 +1,30 @@
+class Room
+
+attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @guests = []
+    @songs = []
+  end
+
+  def guests_in_room
+    return @guests.length
+  end
+
+  # def song_running
+  #   return @songs.count
+  # end
+  #
+  # def check_in_guest(guest)
+  #   @guests.push(guest)
+  # end
+  #
+  # def check_out_guest(guest)
+  #   @guests.delete(guest)
+  # end
+  #
+  # def check_in_song(song)
+  #   @songs.push(song)
+  # end
+end
---
+++ specs/room_spec.rb
@@ -0,0 +1,47 @@
+require('minitest/autorun')
+require('minitest/rg')
+require_relative('../room.rb')
+require_relative('../guest.rb')
+require_relative('../song.rb')
+
+class TestRoom < MiniTest::Test
+
+  def setup
+    @room1 = Room.new('Rock')
+    @room2 = Room.new('Pop')
+    @guest1 = Guest.new('Andrew')
+    @guest2 = Guest.new('John')
+    @guest3 = Guest.new('William')
+    @guest4 = Guest.new('Jake')
+    @song1 = Song.new('One', 'Metallica')
+    @song2 = Song.new('Duke Nukem', 'Megadeth')
+    @song3 = Song.new('Davidian', 'Machine Head')
+  end
+
+
+  def test_get_room_name
+    assert_equal('Rock', @room1.name)
+  end
+
+  def test_guests_in_room
+    assert_equal(0, @room1.guests_in_room)
+  end
+
+  # def test_check_in_guest
+  #   @room1.check_in_guest(@guest1)
+  #   assert_equal(1, @room1.guests_in_room)
+  # end
+  #
+  # def test_check_out_guest
+  #   @room1.check_in_guest(@guest1)
+  #   @room1.check_in_guest(@guest2)
+  #   @room1.check_out_guest(@guest1)
+  #   assert_equal(1, @room1.guests_in_room)
+  # end
+  #
+  # def test_check_in_song
+  #   @room1.check_in_song(@song1)
+  #   assert_equal(1, @room1.song_running)
+  # end
+
+end
